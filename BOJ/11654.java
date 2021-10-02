e18f5673d75c973ed2fbd223f170b71648c70d11
JuyoungIt <21700760@handong.edu>
2021-10-02 10:29:51 +0900

    commit Q.11654 solution

---
+++ BOJ/11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner key = new Scanner(System.in);
+		String str = key.nextLine();
+		char let = str.charAt(0);
+
+		System.out.println((int)let);
+	}
+}
