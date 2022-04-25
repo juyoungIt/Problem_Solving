5e09b84231559fde631540e280154b8aaa9357ee
juyoungit <21700760@handong.edu>
2022-04-25 21:36:40 +0900

    commit BOJ 2644 solution

--- DFS/BOJ_2644.java
+++ DFS/BOJ_2644.java
@@ -5,58 +5,53 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int n;  // the number of whole people
-	static int t1; // target1
-	static int t2; // target2
-	static int m;  // the number of relations
-	static int[][] r; // the relation information
-	static int[][] am; // adjaceny matrix
-	static boolean[] v; // store the visit info
-	static int count = 0; // store the 'chon' count
-	static boolean isFound = false;
+	static int n; // number of people
+	static int[][] relation; // relation between each people
+	static int result = -1;   // store the 'chon' value
+	static int[][] am; // adjacency matrix
+	static boolean[] visit; // store the visit information
 
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
 		n = key.nextInt();
-		t1 = key.nextInt();
-		t2 = key.nextInt();
-		m = key.nextInt();
-		am = new int[n+1][n+1];
-		v = new boolean[n+1];
-		r = new int[m][2];
+		int target1 = key.nextInt();
+		int target2 = key.nextInt();
+		int m = key.nextInt(); // the number of relations
+		relation = new int[m][2];
 		for(int i=0 ; i<m ; i++) {
-			r[i][0] = key.nextInt();
-			r[i][1] = key.nextInt();
+			relation[i][0] = key.nextInt();
+			relation[i][1] = key.nextInt();
 		}
 
 		// build adjacency matrix
+		am = new int[n+1][n+1];
+		visit = new boolean[n+1];
 		for(int i=0 ; i<m ; i++) {
-			am[r[i][0]][r[i][1]] = 1;
-			am[r[i][1]][r[i][0]] = 1;
+			am[relation[i][0]][relation[i][1]] = 1;
+			am[relation[i][1]][relation[i][0]] = 1;
 		}
 
-		// perform the dfs search
-		dfs(t1, t2);
+		// perform the dfs
+		dfs(target1, target2, 0);
 
 		// print out the result
-		if(isFound) System.out.println(count);
-		else System.out.println("-1");
+		System.out.println(result);
 
 		key.close();
 		System.exit(0);
 	}
 
-	public static void dfs(int sn, int en) {
-		if(sn == en)
-			isFound = true;
-		v[sn] = true;
+	public static void dfs(int sn, int en, int count) {
+		
+		if(sn == en) {
+			result = count;
+			return;	
+		}
+		
+		visit[sn] = true;
 		for(int i=1 ; i<n+1 ; i++) {
-			if(isFound)
-				break;
-			if(am[sn][i] == 1 && !v[i]) {
-				count++;	
-				dfs(i, en);
-			}
+			if(am[sn][i] == 1 && !visit[i])
+				dfs(i, en, count+1);
 		}
 	}
 }
